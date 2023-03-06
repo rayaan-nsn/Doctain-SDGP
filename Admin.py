d596926382d3ada6a8bbcc7283d3c58063d253eb
Senal Fernando <senal3fernando@gmail.com>
2023-03-06 15:14:35 +0530

    updated flask to get form details to backend

--- Admin.py
+++ Admin.py
@@ -1,14 +1,27 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request , redirect, url_for
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('AdminPage.html')
 
-@app.route('/addDoctor')
+@app.route('/addDoctor',methods=["GET","POST"])
 def addDoctor():
    return render_template('AddDoctor.html')
+
+@app.route('/save', methods=["GET","POST"])
+def save():
+
+    userInput = request.form
+    return render_template('test.html', userInput=userInput)
+    
+
+# #method to send doctor register form data to backend
+# @app.route('/handle_data', methods=['POST'])
+# def handle_data():
+#    projectpath = request.form['projectFilepath']
 
 if __name__ == '__main__':
    app.run(debug=True)
--- User.py
+++ User.py
@@ -25,3 +25,5 @@ def receive_user_input():
 
 if __name__ == '__main__':
    app.run(debug=True)
+
+
