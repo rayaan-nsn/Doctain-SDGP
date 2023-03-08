953e2258f0ce25d472bd4c69f39e1fafe01930a6
rayaan-nsn <rayaannsn@gmail.com>
2023-03-08 20:15:44 +0530

    prediction model integration

--- User.py
+++ User.py
@@ -1,37 +1,83 @@
 import os
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-   return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/faq')
 def faq():
-   return render_template('faq.html')
+    return render_template('faq.html')
+
 
 @app.route('/contactUs')
 def contactUs():
-   return render_template('contact.html')
+    return render_template('contact.html')
+
 
 @app.route('/user_input', methods=['POST'])
 def receive_user_input():
-    global input_value, diseases, doctors
     if request.method == 'POST':
-        user_input = request.json  # assuming input is sent as a JSON array
-        # do something with user input, e.g. store it in a database
-        for input_value in user_input:
-            print(input_value)
-        if len(input_value) > 0:
-            diseases = ['High Blood Pressure', 'Diabetes', 'Asthma', 'Migraine']
-            doctors = ['Dr. Patel', 'Dr. Namal', 'Dr. John']
+        user_symptoms = request.json  # assuming input is sent as a JSON array
+        predicted_disease, matching_diseases = predict_disease(user_symptoms)
+        doctors = ['Dr. Patel', 'Dr. Fernando', 'Dr. John']
         return jsonify({
             'message': 'User input received successfully.',
-            'disease': diseases,
+            'predicted_disease': predicted_disease,
+            'matching_diseases': matching_diseases,
             'doctors': doctors
         })
 
+
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
 
 
+def predict_disease(user_symptoms):
+    # Load the training and the testing dataset from the CSV file
+    train_dataset = pd.read_csv('./static/dataset/training_dataset (non_com).csv')
+    test_dataset = pd.read_csv('./static/dataset/testing_dataset (non_com).csv')
+
+    # Separate the symptoms from the disease labels in the training dataset and in the testing dataset
+    X_train = train_dataset.iloc[:, 1:]
+    y_train = train_dataset.iloc[:, 0]
+    X_test = test_dataset.iloc[:, 1:]
+    y_test = test_dataset.iloc[:, 0]
+
+    # Create a Naive Bayes classifier and fit it to the training data
+    model = GaussianNB()
+    model.fit(X_train, y_train)
+
+    # Use the model to predict the disease based on user input
+    symptoms_dict = {symptom: 1 for symptom in user_symptoms}
+    user_input = pd.DataFrame(symptoms_dict, index=[0])
+    missing_cols = set(X_train.columns) - set(user_input.columns)
+    for col in missing_cols:
+        user_input = pd.concat([user_input, pd.DataFrame({col: [0]})], axis=1)
+    user_input = user_input[X_train.columns]
+
+    # Predict the probability of each disease given the user input
+    disease_probs = model.predict_proba(user_input)[0]
+    disease_indices = model.classes_
+
+    # Sort the probabilities in descending order and get the top 5 predicted diseases
+    n_top = 5
+    top_indices = disease_probs.argsort()[::-1][:n_top]
+
+    # Get the most likely disease and matching other diseases
+    predicted_disease = disease_indices[top_indices[0]]
+    matching_diseases = []
+    for i in range(1, n_top):
+        if disease_probs[top_indices[i]] > 0:
+            matching_diseases.append(disease_indices[top_indices[i]])
+
+    print(predicted_disease)
+    print(matching_diseases)
+
+    return predicted_disease, matching_diseases
--- static/js/home.js
+++ static/js/home.js
@@ -1,83 +1,58 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  var userInputArray = [];  // Define an empty array to store user input
+   var userInputArray = []; // Define an empty array to store user input
 
-  // When user types in the search input field  
-  $('#searchTerm').on('keyup', function() {
+   // When user types in the search input field
+   $('#searchTerm').on('keyup', function () {
       var input = $(this).val().toLowerCase();
 
       //suggestion list
-      var suggestions = [  "Chest pain", "Shortness of breath", "Fatigue", "Dizziness", "Sweating", "Nausea",
-          "Vomiting", "Lightheadedness", "Weakness", "Irregular heartbeat", "Headaches", "Blurred vision", "Distorted vision",
-          "Nosebleeds", "Palpitations", "Feeling of fullness or pressure in chest", "Leg pain", "Numbness", "Tingling",
-          "Coldness", "Slow healing of wounds", "Swelling in legs and ankles", "Swelling", "Persistent cough",
-          "Rapid or irregular heartbeat", "Cough", "Fainting", "Fever", "Night sweats", "Weight loss", "Weight gain",
-          "Blood-tinged sputum", "Seizures", "Sudden loss of consciousness", "No pulse", "No breathing",
-          "Stiffness or tightness in the joints of the hands", "Swollen ankles and legs", "Extreme thirst",
-          "Frequent urination", "Sudden weight loss", "Increased hunger", "Dry mouth", "Slow-healing wounds",
-          "High blood pressure", "Swollen feet and ankles", "Fruity-smelling breath", "Rapid breathing", "Confusion",
-          "Abdominal pain", "High fever", "Vision changes", "Dry skin", "Shaking", "Anxiety", "Irritability",
-          "High blood sugar levels in the morning", "Floaters", "Blind spots", "Difficulty seeing at night", "Vision loss",
-          "Burning in the hands and feet", "Difficulty seeing colors", "Sharp pains", "Sensitivity to touch",
-          "Loss of coordination", "Sores, cuts, or blisters that don't heal", "Redness", "Thickening of the skin",
-          "Poor circulation", "Dry, itchy skin", "Skin infections", "Patches of dark skin", "Bleeding gums",
-          "Red or swollen gums", "Unconsciousness", "Bad breath", "Receding gums", "Loose teeth", "Labored breathing",
-          "Acetone breath", "Bloating", "Heartburn", "Difficulty achieving or maintaining an erection", "Reduced sexual desire",
-          "Reduced sensation in the penis", "Pain, warmth or discharge from the affected area",
-          "High levels of triglycerides and low-density lipoprotein (LDL) cholesterol",
-          "Low levels of high-density lipoprotein (HDL) cholesterol", "Patches of light brown or scaly skin",
-          "Small raised bumps that look like pimples", "Inability to fully extend the fingers", "Thick, tight skin on the hands",
-          "chills", "chest discomfort", "reduced tolerance for exercise", "fingernail clubbing", "swollen lymph nodes",
-          "skin rash", "joint pain", "coughing up blood", "pleural effusion", "pneumonia", "pulmonary embolism", "breast lump",
-          "changes in breast shape or size", "nipple discharge", "changes in the skin of the breast", "swollen lymph nodes",
-          "breast pain", "difficulty starting or stopping urination", "weak or interrupted flow of urine", "blood in urine or semen",
-          "increased urination", "pain in the hips, pelvis, or lower back", "changes in bowel habits", "rectal bleeding or blood in stool",
-          "abdominal pain or cramps", "changes in skin color or texture", "changes in the size, shape or color of a mole",
-          "a sore that does not heal", "a spot or lump that bleeds or crusts over", "abdominal bloating or swelling", "pelvic pain",
-          "difficulty eating or feeling full quickly", "loss of appetite", "increased appetite", "jaundice", "dark urine",
-          "foamy or bloody urine", "light-colored stools", "frequent infections", "easy bleeding or bruising", "itching", "headaches",
-          "seizures", "difficulty thinking", "weakness or paralysis in one part of the body", "personality or behavior changes",
-          "difficulty swallowing", "hoarseness", "feeling full after eating a small amount of food", "anemia", "back or pelvic pain",
-          "lump in the neck", "pain in the throat or neck", "feeling of heaviness in the scrotum", "vaginal bleeding after menopause",
-          "abnormal vaginal bleeding between periods", "pain during intercourse", "pain in the bone or joint", "swelling in the affected area",
-          "fractures with little or no trauma", "sore or lump in the mouth, tongue or lip", "changes in the color or texture of the mouth or lips",
-          "coughing up blood", "muscle weakness", "increased hair growth", "wheezing", "chronic cough with mucus", "cough with mucus",
-          "difficulty breathing", "chest discomfort", "dry cough", "chest infections", "poor growth", "bowel obstruction", "erectile dysfunction",
-          "Abdominal pain or discomfort", "light-colored stools", "indigestion", "Blood in urine", "painful or frequent urination",
-          "swelling in the feet", "Abdominal pain or swelling", "hoarseness or voice changes", "recurring respiratory infections",
-          "Lump or swelling in the testicle", "hiccups", "pain or discomfort in the testicle or scrotum", "feeling of heaviness in the scrotum",
-          "back pain", "Loud snoring", "excessive daytime sleepiness", "restless sleep", "abnormal vaginal discharge",
-          "Swelling or lump in the affected area", "Bone pain or tenderness", "thirst", "changes in the color or texture of the skin",
-          "difficulty chewing swallowing or speaking", "chronic cough", "Lump or sore that does not heal", "difficulty swallowing or speaking",
-          "changes in the voice or hoarseness", "pain in the ear, face or neck", "chest tightness", "difficulty breathing"
+      var suggestions = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain',
+         'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition', 'spotting_ urination', 'fatigue',
+         'weight_gain', 'anxiety', 'cold_hands_and_feets', 'mood_swings', 'weight_loss', 'restlessness', 'lethargy', 'patches_in_throat',
+         'irregular_sugar_level', 'cough', 'high_fever', 'sunken_eyes', 'breathlessness', 'sweating', 'dehydration', 'indigestion',
+         'headache', 'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain', 'constipation',
+         'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload',
+         'swelling_of_stomach', 'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm', 'throat_irritation',
+         'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs', 'fast_heart_rate',
+         'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps',
+         'bruising', 'obesity', 'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails',
+         'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech', 'knee_pain', 'hip_joint_pain',
+         'muscle_weakness', 'stiff_neck', 'swelling_joints', 'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness', 'weakness_of_one_body_side',
+         'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine', 'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)',
+         'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain', 'abnormal_menstruation', 'dischromic _patches',
+         'watering_from_eyes', 'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum', 'rusty_sputum', 'lack_of_concentration', 'visual_disturbances',
+         'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen', 'history_of_alcohol_consumption',
+         'fluid_overload', 'blood_in_sputum', 'prominent_veins_on_calf', 'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling',
+         'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose', 'yellow_crust_ooze'
       ];
 
       // Filter the suggestions based on the user input
-      var filteredSuggestions = suggestions.filter(function(suggestion) {
-        return suggestion.toLowerCase().includes(input);
+      var filteredSuggestions = suggestions.filter(function (suggestion) {
+         return suggestion.toLowerCase().includes(input);
       });
 
       var suggestionsHtml = ''; // Define an empty string to store the suggestions HTML
 
       if (input.length > 0) {
-        for (var i = 0; i < filteredSuggestions.length; i++) {  //If user input is not empty loop through the filtered suggestions
-          var suggestion = filteredSuggestions[i];  // Get a suggestion
-          var highlightIndex = suggestion.toLowerCase().indexOf(input); // Get the index of the user input within the suggestion
-          var highlightedSuggestion = suggestion.slice(0, highlightIndex) + '<span class="highlight">' + suggestion.slice(highlightIndex, highlightIndex + input.length) + '</span>' + suggestion.slice(highlightIndex + input.length); // Highlight the user input within the suggestion using a span with a class of 'highlight'
-          suggestionsHtml += '<div class="suggestion">' + highlightedSuggestion + '</div>'; // Add the highlighted suggestion to the suggestions HTML
-        }
+         for (var i = 0; i < filteredSuggestions.length; i++) { //If user input is not empty loop through the filtered suggestions
+            var suggestion = filteredSuggestions[i]; // Get a suggestion
+            var highlightIndex = suggestion.toLowerCase().indexOf(input); // Get the index of the user input within the suggestion
+            var highlightedSuggestion = suggestion.slice(0, highlightIndex) + '<span class="highlight">' + suggestion.slice(highlightIndex, highlightIndex + input.length) + '</span>' + suggestion.slice(highlightIndex + input.length); // Highlight the user input within the suggestion using a span with a class of 'highlight'
+            suggestionsHtml += '<div class="suggestion">' + highlightedSuggestion + '</div>'; // Add the highlighted suggestion to the suggestions HTML
+         }
       }
-      $('#suggestionsList').html(suggestionsHtml);  // Set the suggestions HTML to the suggestions list element
-    });
-  
+      $('#suggestionsList').html(suggestionsHtml); // Set the suggestions HTML to the suggestions list element
+   });
 
-    // When a suggestion is clicked
-    $(document).on('click', '.suggestion', function() {
+
+   // When a suggestion is clicked
+   $(document).on('click', '.suggestion', function () {
       var tag = $(this).text(); // Get the text of the clicked suggestion
 
       // If the tag is not already in the tag list, add it
       if ($('#tagList').find('.tag:contains(' + tag + ')').length == 0) {
-        $('#tagList').append('<div class="tag">' + tag + '&nbsp;&nbsp;&nbsp;<i class="fa-regular fa-circle-xmark close"></i></div>');
+         $('#tagList').append('<div class="tag">' + tag + '   <i class="fa-regular fa-circle-xmark close"></i></div>');
       }
 
       // Clear the search input field and the suggestions list
@@ -86,78 +61,80 @@ $(document).ready(function() {
 
       userInputArray.push(tag); // Add the tag to the user input array
 
-    });
+   });
 
 
-
-  // When a key is pressed down on the search input field
-    $('#searchTerm').on('keydown', function(e) {
+   // When a key is pressed down on the search input field
+   $('#searchTerm').on('keydown', function (e) {
       if (e.which === 13) {
-        var tag = $(this).val();  // If the key is the Enter key get the value of the search input field
+         var tag = $(this).val(); // If the key is the Enter key get the value of the search input field
 
-        // If the tag is not already in the tag list, add it
-        if ($('#tagList').find('.tag:contains(' + tag + ')').length == 0) {
-            $('#tagList').append('<div class="tag">' + tag + '&nbsp;&nbsp;&nbsp;<i class="fa-regular fa-circle-xmark close"></i></div>');
-        }
+         // If the tag is not already in the tag list, add it
+         if ($('#tagList').find('.tag:contains(' + tag + ')').length == 0) {
+            $('#tagList').append('<div class="tag">' + tag + '   <i class="fa-regular fa-circle-xmark close"></i></div>');
+         }
 
-        // Clear the search input field and the suggestions list
-        $(this).val('');
-        $('#suggestionsList').empty();
+         // Clear the search input field and the suggestions list
+         $(this).val('');
+         $('#suggestionsList').empty();
 
-        userInputArray.push(tag); // Add the tag to the user input array
+         userInputArray.push(tag); // Add the tag to the user input array
       }
-    });
+   });
 
-  
-    // When the close button on a tag is clicked remove the tag from the tag list
-    $(document).on('click', '.tag .close', function() {
+
+   // When the close button on a tag is clicked remove the tag from the tag list
+   $(document).on('click', '.tag .close', function () {
       $(this).parent().remove();
-    });
+   });
 
 
-  $('.searchButton').on('click', function() {
-    $.ajax({
-      type: "POST",
-      url: "/user_input",
-      data: JSON.stringify(userInputArray),
-      contentType: "application/json;charset=UTF-8",
-      success: function(response) {
-        $('.container').empty().append(
+   $('.searchButton').on('click', function () {
+      $.ajax({
+         type: "POST",
+         url: "/user_input",
+         data: JSON.stringify(userInputArray),
+         contentType: "application/json;charset=UTF-8",
+         success: function (response) {
+            $('.container').empty().append(
 
-            `<div class="results-container">
+               `<div class="results-container">
                 <div class='disease-column'>
-                    <h1 class="">Possible Causes</h1>
-                    <div class="possible-diseases"></div>
+                    <h1 class="">Predicted Disease</h1>
+                    <div class="predicted_disease"></div>
+                    <div class="matching_diseases"></div>
                 </div>
                 <div class="doctors-column">
                     <h1>Recommended Doctors</h1>
-                    <div class="recommended-doctors">
-</div>
+                    <div class="recommended-doctors"></div>
                 </div>
             </div>
             `);
 
-        for (let i = 0; i < response.disease.length; i++) {
-          const disease = response.disease[i];
-          $('.possible-diseases').append(`<p class="disease">${disease}</p>`);
-        }
-        for (let i = 0; i < response.doctors.length; i++) {
-          const doctors = response.doctors[i];
-          $('.recommended-doctors').append(`
-              <div class="doctor-profile">
-                <h6></h6>
-                <p>${doctors}</p>
-              </div>
-          `);
-        }
-      },
-      error: function(xhr, status, error) {
-        console.log(xhr.responseText);
-      }
-    });
-  });
+            const predicted_disease = response.predicted_disease;
+            $('.predicted_disease').append(`<h4 class="diseases" style="color: red">${predicted_disease}</h4>`);
+
+            for (let i = 0; i < response.matching_diseases.length; i++) {
+               const matching_diseases = response.matching_diseases[i];
+               $('.matching_diseases').append(`<p>${matching_diseases}</p>`);
+            }
 
 
-    
+            for (let i = 0; i < response.doctors.length; i++) {
+               const doctor = response.doctors[i];
+               $('.recommended-doctors').append(`
+                 <div class="doctor-profile">
+                   <h6></h6>
+                   <p>${doctor}</p>
+                 </div>
+               `);
+            }
+         },
+         error: function (xhr, status, error) {
+            console.log(xhr.responseText);
+         }
+      });
+   });
+
+
 });
-
